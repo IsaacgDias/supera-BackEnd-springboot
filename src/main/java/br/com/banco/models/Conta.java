b05cad537ed8bc78846cccc5369b4f7ef66db1ae
isaacgdias <isaacbogui98@gmail.com>
2023-09-27 14:51:51 -0300

    model conta e transferencia

--- src/main/java/br/com/banco/models/Conta.java
+++ src/main/java/br/com/banco/models/Conta.java
@@ -1,4 +1,34 @@
 package br.com.banco.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "conta")
 public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long id;
+
+    @Column(name = "nome_responsavel", length = 50, nullable = false)
+    private String nomeResponsavel;
+
+
+    // Getters e setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+
+    public void setNomeResponsavel(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
 }
