c23c03915fd50ef13ab8a61f804fc9d04209189c
isaacgdias <isaacbogui98@gmail.com>
2023-09-29 18:36:25 -0300

    CORS para permetir solicitaçãoes HTTP

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -2,6 +2,9 @@ package br.com.banco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BancoApplication {
@@ -10,4 +13,16 @@ public class BancoApplication {
         SpringApplication.run(BancoApplication.class, args);
     }
 
+    //Resolve o erro No 'Access-Control-Allow-Origin' header is present no navegador do http://localhost:5173 Front-End
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
